9f91eb56fc62809ef8d4389bb97d399778de016b
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-10 00:27:39 +0530

    Assests folder added and styles applied

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './navbar.css'
-
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="This is Logo of Site" />
+        </div>
+        <div className="gpt3__navbar-links_container scale-up-center">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+          <p>Sign In</p>
+          <button type="button">Sign up</button>
+      </div>
+    </div>
   )
 }
 
