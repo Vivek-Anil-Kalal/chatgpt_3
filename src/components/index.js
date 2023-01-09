9f968c8aee9a2a2582f1f230e76d6f5f72ac6bc7
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-09 19:34:51 +0530

    initial commit with all files setup

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// This file is for all the imports which is required any where in out application
+export {default as Article} from './article/Article'
+export {default as Brand} from './brand/Brand'
+export {default as CTA} from './cta/CTA'
+export {default as Navbar} from './navbar/Navbar'
