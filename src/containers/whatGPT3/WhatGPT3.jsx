666f2e5c2a8cb0e31d8b6f7eb47862284cabf443
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-20 23:14:12 +0530

    Brand component is Completed

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Features from '../features/Features'
 import './whatgpt3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-features">
+        <Features />
+      </div>
+    </div>
   )
 }
 
