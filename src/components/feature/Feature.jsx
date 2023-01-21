0e6f404143c4097dea14331264e8a0bba1121f6f
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-01-21 10:25:46 +0530

    What GPT3 Completed

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title , text}) => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        {/* Like this we can add empty div to add empty space  */}
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p> {text}</p>
+      </div>
+    </div>
   )
 }
 
